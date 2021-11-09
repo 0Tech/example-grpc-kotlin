d0a9740cf01c39b06ebbffd0acd75a800a1d2429
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2021-11-09 17:07:37 +0900

    Add version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
+pluginManagement {
+    plugins {
+        id("com.google.protobuf") version "0.8.17"
+    }
+}
+
+enableFeaturePreview("VERSION_CATALOGS")
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.versions.toml"))
+        }
+    }
+}
+
 rootProject.name = "example-grpc-kotlin"
 include(
 )
